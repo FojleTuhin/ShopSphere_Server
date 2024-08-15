dc15f542e8223b575c6f8c7816d2dec723895280
fojletuhin <fojletuhin@gmail.com>
2024-08-16 01:03:15 +0600

    git init

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.port || 5000;
+app.use(express.json());
+app.use(cors({
+    origin:"*"
+  }))
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z7hla77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("ShopSphere").collection('products');
+
+    app.get('/products', async(req, res)=>{
+        const result = await productsCollection.find().toArray();
+        res.send(result);
+    })
+
+
+   
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello from ShopSphere');
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  })
